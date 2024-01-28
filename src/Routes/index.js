cd91a1f95016fafb838b6a5345a1134c0ed3dbee
thanhctag99 <thanhctag1999@gmail.com>
2024-01-28 16:41:13 +0700

    change file name

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,7 +1,7 @@
 import Agent from "../pages/Agents/Agents";
 import AgentDetail from "../pages/Agents/Agent_Detail";
 import Dashboard from "../pages/Dashboard/Dashboard";
-import Maps from "../pages/Maps/maps";
+import Maps from "../pages/Maps/Maps";
 import PlayerCards from "../pages/Player_Card/Player_cards";
 import Weapons from "../pages/Weapons/Weapons";
 
